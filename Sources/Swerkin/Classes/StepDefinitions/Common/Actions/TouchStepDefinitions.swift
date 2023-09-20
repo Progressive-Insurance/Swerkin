61e52bc9f9f94da7572a265d91c80848f4af06e8
Rhishi Katoch <Rhishi_Katoch@progressive.com>
2023-09-20 10:33:03 -0400

    SPM support plus pipeline updates

--- Sources/Swerkin/Classes/StepDefinitions/Common/Actions/TouchStepDefinitions.swift
+++ Sources/Swerkin/Classes/StepDefinitions/Common/Actions/TouchStepDefinitions.swift
@@ -5,6 +5,10 @@
  All Rights Reserved.Use of this source code is governed by an MIT license that can be found in the LICENSE file at https://github.com/Progressive/Swerkin/LICENSE.md
  */
 
+import CoreFoundation
+import CoreGraphics
+import XCTest
+
 extension Steps {
 
     /// Touch element with given accessibility label
