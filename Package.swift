dd17945a12d628a6dc1b55bfbd4799a7ee8cb099
Rhishi Katoch <Rhishi_Katoch@progressive.com>
2025-02-07 10:24:55 -0500

    Point Kif to master

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/kif-framework/KIF.git", .exact("3.9.0"))],
+        .package(url: "https://github.com/kif-framework/KIF.git", .branch("master"))],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
